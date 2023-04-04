89bf41a8152b5542b6b579e6c1df69161141a690
Stanislav <stanislav.bereza@spglobal.com>
2023-04-04 19:20:47 +0300

    add servers to open api documents builder

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,20 @@ import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 
 let server: Handler;
+const LOCAL_SERVER_URL = 'http://localhost:3000/dev';
 
 async function bootstrap(): Promise<Handler> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('CloudX Database')
     .setDescription('CloudX Database API Documentation')
     .setVersion('1.0')
+    .addServer(LOCAL_SERVER_URL)
+    .addServer(process.env.DEV_SERVER_URL)
     .build();
 
-  const document = SwaggerModule.createDocument(app, config, { ignoreGlobalPrefix: true });
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
   await app.init();
@@ -23,7 +26,7 @@ async function bootstrap(): Promise<Handler> {
   const expressApp = app.getHttpAdapter().getInstance();
 
   return serverlessExpress({ app: expressApp });
-}
+};
 
 export const handler: Handler = async (
   event: any,
